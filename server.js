e9fc4e02b9b936518c66ef573751f9870e4c8c43
idalmys <idalmysmaza@gmail.com>
2020-06-13 19:25:34 -0500

    deploying

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express= require("express")
 const mongoose= require("mongoose")
-const routes = require("./routes");
+const routes = require("./routes/index");
+
+var Book=(require("./models/Book"))
 //Settings
 const app=express();
 const PORT=process.env.PORT || 3001;
