d7157937fa9cea1f9554c5256e9c82a8bcfc6a75
idalmys <idalmysmaza@gmail.com>
2020-06-13 20:59:11 -0500

    again

--- routes/apiBooks.js
+++ routes/apiBooks.js
@@ -1,5 +1,6 @@
 const router= require("express").Router();
 const Book=require("../controllers/bookControllers")
+const path= require("path")
 
 router.route("/")
     .get(Book.getBooks)
@@ -10,5 +11,7 @@ router.route("/:id")
     .put(Book.updateBook)
     .delete(Book.deleteBook)
 
-
+   router.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "/../client/build/index.html"));
+       });
 module.exports=router;
